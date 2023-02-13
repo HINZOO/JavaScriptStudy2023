2f8e006ca816277526d1816ecfb771f7ae6a67ff
HIMJU <hinge1128@gmail.com>
2023-02-13 17:07:17 +0900

    230213

---
+++ JavaScriptStudy/src/main/java/com/acorn/javascriptstudy/EmpDto.java
@@ -0,0 +1,118 @@
+package com.acorn.javascriptstudy;
+
+import java.util.Date;
+
+//mysql -uroot -pmysql123 //use scott // desc emp; // alt 눌러 복붙
+/*+----------+-------------+------+-----+---------+-------+
+| Field    | Type        | Null | Key | Default | Extra |
++----------+-------------+------+-----+---------+-------+
+| EMPNO    | int         | NO   | PRI | NULL    |       |
+| ENAME    | varchar(10) | YES  |     | NULL    |       |
+| JOB      | varchar(9)  | YES  |     | NULL    |       |
+| MGR      | int         | YES  | MUL | NULL    |       |
+| HIREDATE | date        | YES  |     | NULL    |       |
+| SAL      | float(7,2)  | YES  |     | NULL    |       |
+| COMM     | float(7,2)  | YES  |     | NULL    |       |
+| DEPTNO   | int         | YES  | MUL | NULL    |       |
++----------+-------------+------+-----+---------+-------+*/
+/*+--------+-------------+------+-----+---------+-------+
+| Field  | Type        | Null | Key | Default | Extra |
++--------+-------------+------+-----+---------+-------+
+| DEPTNO | int         | NO   | PRI | NULL    |       |
+| DNAME  | varchar(14) | YES  |     | NULL    |       |
+| LOC    | varchar(13) | YES  |     | NULL    |       |
++--------+-------------+------+-----+---------+-------+*/
+public class EmpDto {
+    //DB의 자료를 java 에서 처리하기 위해 받아오는 역할
+    //이때 db의 type과 유사한 java의 type을 선택해야 한다.
+    private int empno; //pk
+    private String ename;
+    private String job;
+    private int mgr;//fk Emp.empno
+    private Date hiredate;
+    private double sal;
+    private double comm; //is null (기본형은 null을 참조할 수 없다.)
+    private int deptno;//is null fk Dept.deptno
+
+
+
+
+
+    public int getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(int empno) {
+        this.empno = empno;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public int getMgr() {
+        return mgr;
+    }
+
+    public void setMgr(int mgr) {
+        this.mgr = mgr;
+    }
+
+    public Date getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(Date hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    public double getSal() {
+        return sal;
+    }
+
+    public void setSal(double sal) {
+        this.sal = sal;
+    }
+
+    public double getComm() {
+        return comm;
+    }
+
+    public void setComm(double comm) {
+        this.comm = comm;
+    }
+
+    public int getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(int deptno) {
+        this.deptno = deptno;
+    }
+
+    @Override
+    public String toString() {
+        return "EmpDto{" +
+                "empno=" + empno +
+                ", ename='" + ename + '\'' +
+                ", job='" + job + '\'' +
+                ", mgr=" + mgr +
+                ", hiredate=" + hiredate +
+                ", sal=" + sal +
+                ", comm=" + comm +
+                ", deptno=" + deptno +
+                '}';
+    }
+}
--- JavaScriptStudy/src/main/java/com/acorn/javascriptstudy/L14EmpList.java
+++ JavaScriptStudy/src/main/java/com/acorn/javascriptstudy/L14EmpList.java
@@ -7,6 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 //톰캣에서 동적페이지를 생성하기 위한 두가지 약속
 //서블릿으로 정의할 타입을 HttpServlet 으로 정의
@@ -18,8 +24,76 @@ public class L14EmpList extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        super.doGet(req, resp);
+        //java.sql.* : JDBC java가 제공하느 db접속 객체들
+        //JDBC가 mysql에 접속하기 위해서는 mysql-j-connector.jar 가 필요하다.
+        String user="root";
+        String pw="mysql123";
+//        String driver ="com.mysql.cj.jdbc.Driver";
+//
+        Connection conn= null;
+        PreparedStatement pstmt=null;
+        ResultSet rs = null;
+        String empListStr="";
+        List<EmpDto> empList = null;
+        try{//try catch 오류가 발생해도 시스템이 멈추지 않게 하는 예외처리
+            Class.forName("com.mysql.cj.jdbc.Driver");//동적로딩의 대상
+            // 동적 로딩 : 특정 객체(DriverManager)가 작업을 수행할 때, 객체를 생성하는 행위 (제어의 역전)
+            // 일반적인 객체지향 언어는 객체를 프로그래머가 직접 생성후 매개변수로 전달.
+            /* 본래 프로그램은 프로그래머에 의해 객체를 생성하지만 동적로딩은 드라이버매니저객체가 직접 객체를 생성한다 ->제어의 역전 */
+
+            //DriverManager가 db에 접속할 때 주소를 보고 필요한 라이브러리를 찾아서 생성후 접속한다.
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/scott",user,pw);
+            pstmt = conn.prepareStatement("SELECT * FROM EMP");
+            rs=pstmt.executeQuery();//질의어를 실행하는 함수
+            empList=new ArrayList<EmpDto>();
+//            PreparedStatement deptnoName = conn.prepareStatement("SELECT loc From dept inner join emp on emp.deptno=dept.deptno");
+
+            while(rs.next()){//tavle row =레코드=튜플=객체
+
+                /* 문자열로 db의 데이터를 받으면 제어할 방법이 없다. 그래서 DTO를 정의해서 사용한다.
+                empListStr+=rs.getInt("empno");
+                empListStr+=rs.getString("ename");
+                empListStr+=rs.getString("job");
+                */
+                EmpDto emp = new EmpDto();
+                emp.setEmpno(rs.getInt("empno"));
+                emp.setEname(rs.getString("ename"));
+                emp.setJob(rs.getString("job"));
+                emp.setSal(rs.getDouble("sal"));
+                emp.setComm(rs.getDouble("comm"));
+                emp.setMgr(rs.getInt("mgr"));
+                emp.setDeptno(rs.getInt("deptno"));
+                emp.setHiredate(rs.getDate("hiredate"));
+                empList.add(emp);
+
+            }
+        }catch (Exception e){// 부모타입의 예외를 작성할 수록 더 많은 예외처리가능.
+            // Exception  : 모든 예외처리 가능
+            e.printStackTrace();
+        }
+
+
+        resp.setContentType("text/html;charset=UTF-8;");
         PrintWriter out = resp.getWriter();//  resp.getWriter() : 리소스로 반환될 문자열 작성
+        //해당 문자열을 리소스로 반환하면 브라우저가 자동으로 문서의 형식을 html로 인지하는데
+        //이때 인코딩이 없어서 브라우저가 인지하는 인코딩이 다른 값으로 나오는것.
         out.println("<h1>Scott.emp list 출력</h1>");
+        out.println("<h2>문제1: 부서번호를 파라미터로 보내면 쿼리로 부서번호에 해당하는 사원만 출력하세요.</h2>");
+        out.println("<h2>문제2: deptno와 empno를 조인해서 부서이름과 상사이름을 출력하세요.</h2>");
+        out.println("<h2><a href='./l14_dept_list.do'>문제3: 부서리스트를 출력하는 동적 페이지를 만드세요</a></h2>");
+        empListStr+="<table>";
+        empListStr+="<tr><th>사번</th><th>이름</th><th>부서번호</th><th>부서이름</th><th>상사이름</th></tr>";
+        for(EmpDto emp : empList){
+            empListStr+="<tr>";
+            empListStr+="<td>"+emp.getEmpno()+"<td>";
+            empListStr+="<td>"+emp.getEname()+"<td>";
+            empListStr+="<td>"+emp.getDeptno()+"<td>";
+            empListStr+="<td>"+"deptName"+"<td>";
+            empListStr+="<td>"+"상사이름조인"+"<td>";
+            empListStr+="</tr>";        }
+        empListStr+="</table>";
+        out.println(empListStr);
+        //java 문서가 바뀌면 class로 컴파일하고 톰캣에 배포된 webapp을 바꿔야한다 (배포!)
     }
 
 
